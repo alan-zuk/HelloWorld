7388a5bf4f4264c3e9ba9815294e393dda09a79d
LABS\usuario11 <tomas.perotti@github.com>
2018-03-19 09:22:37 -0300

    -Agrego contador y print

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,6 +6,11 @@ public class HelloWorld {
 		
 	System.out.println("Hello World Ejemplo. Pobre Alan");
 	System.out.print("Agrego esta linea");
+	
+	//Branch 'Tomas'
+	
+	int contador=1;
+	System.out.println("el numero es: "+contador);
 
 	}
 
