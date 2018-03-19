5cabeda312a18c7a2fd1f642d87254de2483ff19
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-03-19 09:12:02 -0300

    ACA ALAN

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World Ejemplo. Pobre Alan");
-		System.out.print("Agrego esta linea");
+		System.out.println("Hello World Ejemplo. Pobre Alan, LTA");
+		System.out.print("Agrego esta linea y esta");
 
 	}
 
