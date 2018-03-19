dcb84d79545900e4fd81e1f579975a18732f698f
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-03-19 09:26:18 -0300

    Revert "aca commitiando"
    This reverts commit fb835d6dffffb56d89a75c58fbb1879c03d1f738.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,15 +4,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+<<<<<<< HEAD
 		System.out.println("Hello World Ejemplo. Pobre Alan");
 		System.out.print("Agrego esta linea");
 
-
+=======
 		System.out.println("Hello World Ejemplo. Pobre Alan, LTA");
 		System.out.print("Agrego esta linea y esta");
 		//Un comentario no puede romper nada
-
+>>>>>>> d00ce89d56a93bb2b08cfbadead57c83d47aca41
 	}
 
 }
